57765f8ce23f5c9cbcfa655d6443953f24ab91e1
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-05-03 13:49:28 -0700

    Update pixray.py

--- pixray.py
+++ pixray.py
@@ -712,7 +712,8 @@ def do_init(args):
                 if args.init_image_alpha and args.init_image_alpha >= 0:
                     top_image.putalpha(args.init_image_alpha)
                 cur_start_image = starting_image.copy()
-                cur_start_image.paste(top_image, (0, 0), top_image)
+                # cur_start_image.paste(top_image, (0, 0), top_image)
+                cur_start_image.paste(top_image)
                 init_image_rgba_list.append(cur_start_image)
 
             starting_image = init_image_rgba_list[0]
