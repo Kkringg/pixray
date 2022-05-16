5aeae0c26ed78d82fc9e350659a26f8c0aa11b97
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-05-15 19:44:36 -0700

    Update pixray.py

--- pixray.py
+++ pixray.py
@@ -684,10 +684,20 @@ def do_init(args):
 
     # Image initialisation\
     if args.init_noise == 'fft':
+        shape = [1, 3, args.size[1], args.size[0]]
+        test_params_shape = [*shape[:3], shape[3]//2+1, 2] # [1,3,512,257,2] for 512x512 (2 for imaginary and real components)
+        test_params = 0.01 * torch.randn(*test_params_shape).cuda()
+        
+        img1 = test_params[0]
+        save_image(img1, 'test_image.png') 
+        
+        test_imag_t = params.requires_grad_(True)
+        
+        
         #drawer.init_from_tensor(init_tensor=None)
-        test_params = drawer.get_fft_noise(init_tensor=None)
-        test_img = TF.to_pil_image(test_params.cpu())
-        imageio.imwrite('test_image.png', np.array(test_img))
+        #test_params = drawer.get_fft_noise(init_tensor=None)
+        #test_img = TF.to_pil_image(test_params.cpu())
+        #imageio.imwrite('test_image.png', np.array(test_img))
         
         #test_image = drawer.to_image()
         #out = drawer.synth(cur_iteration=-1)
