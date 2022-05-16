9f45b3dc4ceac1460e2dfb8c48f0d18dea845aaa
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-05-15 20:13:30 -0700

    Update pixray.py

--- pixray.py
+++ pixray.py
@@ -685,11 +685,13 @@ def do_init(args):
     # Image initialisation\
     if args.init_noise == 'fft':
         test_shape = [1, 3, args.size[1], args.size[0]]
+        test_shape_2 = torch.rand(1, 3, args.size[1], args.size[0])
+        
         test_params_shape = [*test_shape[:3], test_shape[3]//2+1, 2] # [1,3,512,257,2] for 512x512 (2 for imaginary and real components)
         test_params = 0.01 * torch.randn(*test_params_shape).cuda()
         
         img1 = test_params[0]
-        save_image(img1, 'test_image.png') 
+        save_image(test_shape_2, 'test_image.png') 
         
         test_imag_t = params.requires_grad_(True)
         
