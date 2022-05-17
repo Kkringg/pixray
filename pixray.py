33334681988b06254f1c789c0d7c1c9b1d343e7c
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-05-16 18:16:36 -0700

    Update pixray.py

--- pixray.py
+++ pixray.py
@@ -686,7 +686,8 @@ def do_init(args):
     if args.drawer == 'fft':
         test_shape = [1, 3, args.size[1], args.size[0]]
         #test_shape_2 = torch.rand(1, 3, args.size[1], args.size[0])
-        test_shape_2 = torch.randn(1, 3, args.size[1], args.size[0]).cuda()
+        #test_shape_2 = torch.randn(1, 3, args.size[1], args.size[0]).cuda()
+        test_shape_2 = 0.01 * torch.randn(1, 3, args.size[1], args.size[0]).cuda()
         
         test_params_shape = [*test_shape[:3], test_shape[3]//2+1, 2] # [1,3,512,257,2] for 512x512 (2 for imaginary and real components)
         test_params = 0.01 * torch.randn(*test_params_shape).cuda()
