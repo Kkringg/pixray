189e96e6f7b2bafcdf68570844f23ebb3aef8a62
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-07-07 02:32:06 -0700

    Update slip.py

--- slip.py
+++ slip.py
@@ -181,7 +181,7 @@ class SLIP_Base():
 BLIP_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'BLIP')
 # print("APPENDING PATH ", SLIP_PATH)
 sys.path.append(BLIP_PATH)
-from models import blip_feature_extractor
+from blip.models.blip import blip_feature_extractor
 from collections import namedtuple
 
 class BLIP_Base():
