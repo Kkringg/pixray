daba4fcdad03668fad71d73c43ed061262332f8e
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-07-07 10:57:25 -0700

    Update slip.py

--- slip.py
+++ slip.py
@@ -12,6 +12,8 @@ from clip import clip
 
 import collections
 
+from models.blip import blip_feature_extractor
+
 all_slip_models =  ["SLIP_VITS16", "SLIP_VITB16", "SLIP_VITL16",
                     "SLIP_CC3M", "SLIP_CC12M",
                     "SIMCLR_VITS16",
@@ -181,6 +183,7 @@ class SLIP_Base():
         return text_embeddings.unsqueeze(1)
         
 BLIP_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "BLIP/models")
+print(sys.path)
 print(SLIP_PATH)
 print(BLIP_PATH)
 sys.path.append(BLIP_PATH)
