1de0192245fccd0f5b8a0a20f67726b3b1cfb21e
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-07-07 10:42:49 -0700

    Update slip.py

--- slip.py
+++ slip.py
@@ -181,6 +181,7 @@ class SLIP_Base():
         return text_embeddings.unsqueeze(1)
         
 BLIP_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "BLIP/models")
+print(SLIP_PATH)
 print(BLIP_PATH)
 sys.path.append(BLIP_PATH)
 import blip
