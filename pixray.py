9ff6298a0bf0aee0d6136d0e4cb2cd3705181d82
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-07-17 16:17:02 -0700

    Update pixray.py

--- pixray.py
+++ pixray.py
@@ -484,7 +484,7 @@ class MakeCutouts(nn.Module):
                     cutout = kornia.geometry.transform.rescale(cutout, (1/global_aspect_width, 1))
 
             # if cur_iteration % 50 == 0 and _ == 0:
-            print(cutout.shape)
+            # print(cutout.shape)
             TF.to_pil_image(cutout[0].cpu()).save(f"cutout_im_{cur_iteration:02d}_{spot}.png")
 
             cutouts.append(cutout)
