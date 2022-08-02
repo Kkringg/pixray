4ce6611b41d88c8e7b30bc0ab086f60f17a93391
Kkringg <100822991+Kkringg@users.noreply.github.com>
2022-08-02 14:59:49 -0700

    Update pixray.py

--- pixray.py
+++ pixray.py
@@ -1601,7 +1601,7 @@ def train(args, cur_it):
                 if overlay_image_rgba:
                     cur_z_image1.paste(overlay_image_rgba, (0, 0), mask=overlay_image_rgba)
                     cur_z_image1.save("res_init.png")
-                    cur_z_image1.save(f"res_init_{cur_iteration:03d}.png")
+                    # cur_z_image1.save(f"res_init_{cur_iteration:03d}.png")
                     cur_z_tensor = TF.to_tensor(cur_z_image1)
                     new_tensor = cur_z_tensor.to(device).unsqueeze(0)
                     #drawer.reapply_from_tensor(new_tensor * 2 - 1, cur_it)
